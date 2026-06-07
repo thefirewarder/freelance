240619062931bbabff8d0e4678ea2c1daff3b805
thefirewarder <maskedcuber4life@gmail.com>
2026-06-06 22:07:59 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,24 @@
 import { initializeApp }
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 
-import { getAuth }
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+}
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 
-import { getFirestore }
+import {
+    getFirestore
+}
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1SpDBJBP8f-le_6R_1Stqw1VovOJV5Nk",
-  authDomain: "road-renegade.firebaseapp.com",
-  projectId: "road-renegade",
-  storageBucket: "road-renegade.firebasestorage.app",
-  messagingSenderId: "580114510835",
-  appId: "1:580114510835:web:c33aad2cfb2de335a09886"
+    // config
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(firebaseConfig);
 
 export const auth =
     getAuth(app);
@@ -24,51 +26,16 @@ export const auth =
 export const db =
     getFirestore(app);
 
-import {
-    GoogleAuthProvider,
-    signInWithPopup
+const provider =
+    new GoogleAuthProvider();
+
+export async function login(){
+
+    const result =
+        await signInWithPopup(
+            auth,
+            provider
+        );
+
+    return result.user;
 }
-from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-
-import {
-    doc,
-    setDoc
-}
-from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
-
-await setDoc(
-    doc(
-        db,
-        "users",
-        result.user.uid
-    ),
-    {
-        username:
-            result.user.displayName,
-
-        bounty: 0,
-
-        bestScore: 0
-    },
-    {
-        merge: true
-    }
-);
-
-await updateDoc(
-    doc(
-        db,
-        "users",
-        currentUser.uid
-    ),
-    {
-        bounty:
-            increment(score),
-
-        bestScore:
-            Math.max(
-                oldBest,
-                score
-            )
-    }
-);
