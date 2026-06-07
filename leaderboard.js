9f7a8515552d00e6c4ddc388150f10c500da3839
thefirewarder <maskedcuber4life@gmail.com>
2026-06-07 00:23:32 -0400

    Create leaderboard.js

---
+++ leaderboard.js
@@ -0,0 +1,40 @@
+import {
+    db
+}
+from "./firebase.js";
+
+import {
+    collection,
+    query,
+    orderBy,
+    limit,
+    getDocs
+}
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
+const q = query(
+collection(
+  "db",
+  "users"
+),
+orderBy(
+  "bounty",
+  "desc"
+),
+limit(5)
+)
+
+const snapshot = await getDocs(q)
+let html = ""
+let rank = 1
+snapshot.forEach(doc => {
+  const data = doc.data()
+  html += `<p>
+    ${rank}.
+    ${data.alias}
+    -
+    $${data.bounty.toLocaleString()}
+    </p>
+  `
+  rank++
+})
