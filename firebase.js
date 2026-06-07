a3126fe2da826636f850eb9136176d7818ea09c3
thefirewarder <maskedcuber4life@gmail.com>
2026-06-06 21:39:30 -0400

    Import GoogleAuthProvider and signInWithPopup

--- firebase.js
+++ firebase.js
@@ -35,3 +35,9 @@ export const auth =
 
 export const db =
     getFirestore(app);
+
+import {
+    GoogleAuthProvider,
+    signInWithPopup
+}
+from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
