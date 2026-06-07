c9b020f9395e780428239aa31a678031a6899e91
thefirewarder <maskedcuber4life@gmail.com>
2026-06-07 09:38:17 -0400

    Change startMode to alert and modify game start

--- sketch.js
+++ sketch.js
@@ -903,6 +903,6 @@ Visit all required detours.
 
 Use as few cities as possible for a higher score.`
 );
-let startMode = confirm("Let's try an example. Try entering 'Hartford', as it is between the two cities you will have to get between, 'New York' and 'Boston'. After this tutorial, you can always refresh to play again!")
-startGame(startMode)
+let startMode = alert("Let's try an example. Try entering 'Hartford', as it is between the two cities you will have to get between, 'New York' and 'Boston'. After this tutorial, you can always refresh to play again!")
+setTimeout(startGame, 500, true)
 }
