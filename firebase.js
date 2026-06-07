0b7d17ccd9777705d6b3523a079a828dc1b6f370
thefirewarder <maskedcuber4life@gmail.com>
2026-06-06 21:41:21 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,6 @@ from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC1SpDBJBP8f-le_6R_1Stqw1VovOJV5Nk",
   authDomain: "road-renegade.firebaseapp.com",
@@ -22,7 +16,6 @@ const firebaseConfig = {
   appId: "1:580114510835:web:c33aad2cfb2de335a09886"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const app =
