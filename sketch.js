a418da7ad560857d0045aa7fc096a0dabf16dff2
thefirewarder <maskedcuber4life@gmail.com>
2026-06-08 18:32:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -138,7 +138,7 @@ guestBtn.addEventListener("click",function(){
     regGamesPlayed++
     localStorage.setItem("regGames", regGamesPlayed.toString())
     if(regGamesPlayed === 2){
-        alert("This is your last game as a guest! Sign up to rank on our leaderboard and save your scores!")
+        alert("This is your last game as a guest! Continue as a guest to keep playing normally or sign up to rank on our leaderboard and save your scores!")
     }
     document.getElementById(
                 "loginScreen"
