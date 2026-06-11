fdecb7d2a66ae6ba387ca1fbbe9391ab46d886c8
thefirewarder <maskedcuber4life@gmail.com>
2026-06-11 03:25:06 +0000

    e

--- road-renegade/sketch.js
+++ road-renegade/sketch.js
@@ -1038,7 +1038,6 @@ let leftCoords = leftNetwork.map(city2 => [city2.lat, city2.lon])
     }
 );
         }
-        loadVignette()
     setTimeout(function(){
         alert(
 
@@ -1052,6 +1051,7 @@ Cities visited: ${citiesVisited.length}
 Score: ${score}
 
 The authorities lost your trail.`);
+loadVignette()
 if(!collectingBounty){
 
     alert(
